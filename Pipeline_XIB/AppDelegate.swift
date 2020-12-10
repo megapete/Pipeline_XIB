50360db98b065cd3597f48f704dd9e648e4c7631
Peter C Huber <peter@hubersitech.com>
2020-12-10 16:13:33 -0500

    Initial Commit

---
+++ Pipeline_XIB/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Pipeline_XIB
+//
+//  Created by Peter Huber on 2020-12-10.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
